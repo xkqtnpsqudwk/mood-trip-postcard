6166d3d440a7126b9feba8afa8f0c84d3233662b
DayTomer <xkqtnpsqudwk9649@gmail.com>
2026-07-06 17:38:10 +0200

    preview 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,4 +17,16 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
